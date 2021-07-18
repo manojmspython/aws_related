df306d0f928d7f451df57a105348cb44ec7e07cf
manojmspython <ms.manoj0@gmail.com>
2021-07-18 17:33:21 +1000

    policy class add, detach, create

--- iam/iam.py
+++ iam/iam.py
@@ -1,5 +1,6 @@
 import boto3
-from pprint import pprint
+import json
+from datetime import datetime
 
 
 class IamUser:
@@ -31,4 +32,48 @@ class IamUser:
             print('user doesnt exist')
 
 
+
+class IamPolicy:
+    def __init__(self):
+        self.iam = boto3.client('iam')
+
+    def get_policy(self, policy_name):
+        response = self.iam.get_policy(
+            PolicyArn=f'arn:aws:iam::aws:policy/{policy_name}'
+        )
+        return response['Policy']
+
+    def attach_policy(self, policy_name, role):
+        self.iam.attach_role_policy(
+            PolicyArn=f'arn:aws:iam::aws:policy/{policy_name}',
+            RoleName=role
+        )
+
+    def attach_policy(self, policy_name, role):
+        self.iam.detach_role_policy(
+            PolicyArn=f'arn:aws:iam::aws:policy/{policy_name}',
+            RoleName=role
+        )
+
+    def create_policy(self, statements, policy_name):
+        #the below one is how the statement is supposed to look like
+        # [{
+        #     "Effect": "Allow",
+        #     "Action": [
+        #         "dynamodb:DeleteItem",
+        #         "dynamodb:GetItem",
+        #         "dynamodb:PutItem",
+        #         "dynamodb:Scan",
+        #         "dynamodb:UpdateItem"
+        #     ],
+        #     "Resource": resource_arn}]
+        my_managed_policy = {
+            "Version": datetime.now().strftime('%Y-%m-%d'),
+            "Statement": statements
+        }
+        response = self.iam.create_policy(
+            PolicyName=policy_name,
+            PolicyDocument=json.dumps(my_managed_policy)
+        )
+        return response
 
